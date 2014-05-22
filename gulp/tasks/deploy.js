9e6614e9f5d7eb30c3ae9ec9801e2134d4735961
Felipe Amorim <felipe.leusin@gmail.com>
2014-05-22 19:04:38 -0300

    tentando arrumar task de deploy

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -3,7 +3,9 @@
 var gulp = require('gulp'),
     deploy = require('gulp-gh-pages');
 
-gulp.task('deploy', ['clean','build'], function () {
+gulp.task('deploy', ['clean'], function () {
+  gulp.start('build', function () {
    return gulp.src('./dist/**/*')
         .pipe(deploy('https://github.com/dojoapps/saidacerta.com.git'));
+  })
 });
