9769673ea027951f9298d5566a876ccb3e6ee83d
Felipe Amorim <felipe.leusin@gmail.com>
2014-05-22 19:06:52 -0300

    fix deploy louco

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -4,7 +4,7 @@ var gulp = require('gulp'),
     deploy = require('gulp-gh-pages');
 
 gulp.task('deploy', ['clean'], function () {
-  gulp.start('build', function () {
+  return gulp.start('build', function () {
    return gulp.src('./dist/**/*')
         .pipe(deploy('https://github.com/dojoapps/saidacerta.com.git'));
   })
