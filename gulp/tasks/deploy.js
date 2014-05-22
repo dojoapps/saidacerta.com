9cc55dc564a06127ac48b440680fce19cb4f3af2
Felipe Amorim <felipe.leusin@gmail.com>
2014-05-22 19:27:34 -0300

    fixed deploy

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -3,9 +3,7 @@
 var gulp = require('gulp'),
     deploy = require('gulp-gh-pages');
 
-gulp.task('deploy', ['clean'], function () {
-  return gulp.start('build', function () {
+gulp.task('deploy', function () {
    return gulp.src('./dist/**/*')
         .pipe(deploy('https://github.com/dojoapps/saidacerta.com.git'));
-  })
 });
